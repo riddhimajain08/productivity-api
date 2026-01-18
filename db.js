77b0b521ba3e02707251dcb4d104f35779976dc4
Riddhima Jain <riddhimajain2016@gmail.com>
2026-01-18 20:38:49 +0530

    Update db config for deployment

--- db.js
+++ db.js
@@ -1,21 +1,20 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
-// Validating the connection
-pool.connect((err, client, release) => {
+pool.connect((err) => {
     if (err) {
         return console.error('Error acquiring client', err.stack);
     }
-    console.log('Successfully connected to database: ' + process.env.DB_NAME);
-    release();
+    console.log('Connected to database!');
 });
 
 module.exports = {
